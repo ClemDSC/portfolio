5d76c260c68fd1e95a6d9d9b5e78d8b84b2b5454
ClemDSC <clemence.p14@gmail.com>
2023-01-09 20:10:44 +0100

    css

--- src/components/project/ProjectDetail.jsx
+++ src/components/project/ProjectDetail.jsx
@@ -1,13 +1,13 @@
 export default function ProjectDetail(projectList) {
   return (
-    <div className="border-black border-2 bg-white dark:bg-zinc-200 flex flex-col items-center pt-4 pb-2  md:w-[30%]">
-      <img className="w-[80%]" src={projectList.thumbnail} alt={projectList.title} />
-      <p className="font-semibold tracking-wide uppercase bg-[#C3FA4F] py-1 px-2">&lt; {projectList.title} /&gt;</p>
-      <div className="flex gap-2">
-       <p className="text-center"> Technos :</p>
+    <div className="border-black border-2 bg-white dark:bg-zinc-200 flex flex-col items-center max-[768px]:my-2 pt-4 pb-2">
+      <img className="px-4" src={projectList.thumbnail} alt={projectList.title} />
+      <p className="font-semibold tracking-wide uppercase bg-[#C3FA4F] mt-2 py-1 px-2">&lt; {projectList.title} /&gt;</p>
+      <div className="flex gap-2 md:mt-1">
+       <p className="text-center font-medium"> Stack :</p>
        <div className="flex gap-2">
-        {projectList.technos.map((techno) => {
-          return <p>{techno}</p>;
+        {projectList.stacks.map((stack) => {
+          return <p>{stack}</p>;
         })}
         </div>
       </div>
