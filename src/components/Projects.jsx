ddce961c725f3c07790b900620682966911a99a0
ClemDSC <clemence.p14@gmail.com>
2023-01-09 16:19:34 +0100

    install axios to fetch data // local json file in public

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import ProjectDetail from "./project/ProjectDetail";
 
 export default function Projects() {
+    const [projectList, setProjectList] = useState([])
+
+    const fetchData = () => {
+        axios.get("/data/details-projects.json")
+        .then((res) => {
+            setProjectList(res.data.projects);
+        })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <div id="projects" className="relative mt-12 shadow-[10px_10px_0_-2.5px_#c3fa4f] bg-white border-black border-2 h-32">
             <button className="cursor-default absolute left-20 -top-7 shadow-[10px_10px_0_-2.5px_#c3fa4f] bg-white border-black border-2 text-xl px-10 py-3 ">Projects</button>
-            <ProjectDetail />
+            {projectList.map((projectList) => {
+                return <ProjectDetail key={projectList.id} {...projectList} />
+            })}
         </div>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ export default function Skills() {
   return (
     <div id="skills" className="relative mt-5 shadow-[10px_10px_0_-2.5px_#c3fa4f] bg-white border-black border-2 h-32">
           <button className="cursor-default absolute left-20 -top-7 shadow-[10px_10px_0_-2.5px_#c3fa4f] bg-white border-black border-2 text-xl px-10 py-3 ">Skills</button>
-          <div className="absolute top-12 flex">
+          <div className="mt-12">
             <p className="bg-white">Javascript</p>
             <p className="bg-white">Typescript</p>
             <p className="bg-white">Java</p>
